424f19b23997f92eb21e18d6233938613be41923
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2018-03-16 14:24:13 +0100

    [TASK] add some code comments for next steps
    Signed-off-by: Alexander Hofbauer <hofbauer.alexander@gmail.com>

--- Class/Workers/MergeWorker.js
+++ Class/Workers/MergeWorker.js
@@ -26,6 +26,21 @@ module.exports = class MergeWorker {
   go () {
     if (this._ready.local === true && this._ready.cloud === true) {
       LogHandler.debug('MergeWorker started!')
+
+      // this.LocalStorageWorker.StorageInterface.dir().then((dir) => {
+      //   console.log('Local:', dir)
+      // })
+      // this.CloudStorageWorker.StorageInterface.dir().then((dir) => {
+      //   console.log('Cloud:', dir)
+      // })
+
+      /**
+       * + get differences for both local and server filelist and handle those
+       *     files.
+       * + for all other files, that live locally and remote, check hashes and
+       *     download metadata (id, rev, etc.) if hashes are identical. In case
+       *     they are not identical, handle the conflict.
+       */
     }
   }
 }
